68348e1e10b0c8820c53f91a72e0a6747b87f69f
Nguyen Quoc Thinh <thinh.nguyen.cit23@eiu.edu.vn>
2026-08-08 19:30:27 +0700

    đã xoá biến IsSoldByWeight (không dùng) trong GreenBasket.Domain/Entities/Product.cs

--- GreenBasket.Domain/Entities/Product.cs
+++ GreenBasket.Domain/Entities/Product.cs
@@ -16,6 +16,6 @@ namespace GreenBasket.Domain.Entities
         [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
 
-        public bool IsSoldByWeight { get; set; }
+
     }
 }
